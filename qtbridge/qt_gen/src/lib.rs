c07e73d53e572a81e1fcfc9f9c943226092a3f6d
Matthias Rauter <matthias.rauter@qt.io>
2026-03-24 09:52:44 +0100

    Merge qt_gen and qt_gen_macro crates
    As a side effect, qproperty! cannot be a macro_rules! macro anymore,
    since it has to live in a proc-macro. It was converted into a
    proc_macro, having a negative effect on the documentation.
    
    We have to reduce the number of crates for the sake of publishing
    on crates.io. Every sub-crate need to have a public repository on
    crates.io. Currently this would be ~15 crates which we need to find
    names and crates for. This does not scale well. Therefore the crates
    have to be reduced.
    
    Task-number: QTBRIDGES-162
    Change-Id: I3c3f2a8d2151865541a9926eba3afa351c9d4c27
    Reviewed-by: Piotr Wierciński <piotr.wiercinski@qt.io>

--- qtbridge/qt_gen/src/lib.rs
+++ qtbridge/qt_gen/src/lib.rs
@@ -1,118 +1,63 @@
 // Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
-pub use qt_gen_macro;
-pub use qt_gen_macro::*;
 
-#[macro_export]
-macro_rules! qproperty {
+use proc_macro::TokenStream;
+use qt_gen_common::type_qualified_mapping::CallOrigin;
+
+#[proc_macro_attribute]
+pub fn qobject(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut builder = qt_gen_impl::QObjectModuleBuilder::new(CallOrigin::External);
+    let output = match builder.build_token_stream(input.into(), args.into()) {
+        Ok(tokens) => tokens,
+        Err(err) => err.to_compile_error(),
+    };
+    output.into()
+}
+
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn qobject_internal(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut builder = qt_gen_impl::QObjectModuleBuilder::new(CallOrigin::Internal);
+    let output = match builder.build_token_stream(input.into(), args.into()) {
+        Ok(tokens) => tokens,
+        Err(err) => err.to_compile_error(),
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn qobject_impl(args: TokenStream, input: TokenStream) -> TokenStream {
+    let output = match qt_gen_impl::qobject_impl(input.into(), args.into(), &CallOrigin::External) {
+        Ok(o) => o.to_token_stream(),
+        Err(err) => err.to_compile_error(),
+    };
+    output.into()
+}
+
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn qobject_impl_internal(args: TokenStream, input: TokenStream) -> TokenStream {
+    let output = match qt_gen_impl::qobject_impl(input.into(), args.into(), &CallOrigin::Internal) {
+        Ok(o) => o.to_token_stream(),
+        Err(err) => err.to_compile_error(),
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn qsignal(_: TokenStream, _: TokenStream) -> TokenStream {
     // This macro does nothing but offer an entry point for Rust doc
-    (
-        $name:literal
-        , Read = $read:ident
-        $(, Write = $write:ident)?
-        $(, Notify = $notify:literal)?
-        $(, Constant)?
-        $(, Default)?
-        $(,)?
-    ) =>  {{
-        #[cfg(test)]
-        {
-            [
-                $name,
-                "",
-                stringify!($read),
-                test_helper!(@opt $( $write )?),
-                test_helper!(@notify $( $notify )?)
-            ]
-        }
-        #[cfg(not(test))]
-        {
-            compile_error!("qproperty! macro called outside #[qobject] or #[qobject_impl].");
-        }
-    }};
-
-    (
-        $name:literal,
-        Member = $member:ident
-        $(, Read = $read:ident)?
-        $(, Write = $write:ident)?
-        $(, Notify = $notify:literal)?
-        $(, Constant)?
-        $(, Default)?
-        $(,)?
-    ) => {{
-        #[cfg(test)]
-        {
-            [
-                $name,
-                stringify!($member),
-                test_helper!(@opt $( $read )?),
-                test_helper!(@opt $( $write )?),
-                test_helper!(@notify $( $notify )?)
-            ]
-        }
-        #[cfg(not(test))]
-        {
-            compile_error!("qproperty! macro called outside #[qobject] or #[qobject_impl].");
-        }
-    }};
+    panic!("#[qsignal] proc macro called outside #[qobject] or #[qobject_impl].")
 }
 
-#[cfg(test)]
-#[macro_export]
-macro_rules! test_helper {
-    (@opt $val:ident) => {
-        stringify!($val)
-    };
-
-    (@opt) => {
-        ""
-    };
-
-    (@notify $val:literal) => {
-        $val
-    };
-
-    (@notify) => {
-        ""
-    };
+#[proc_macro_attribute]
+pub fn qslot(_: TokenStream, _: TokenStream) -> TokenStream {
+    // This macro does nothing but offer an entry point for Rust doc
+    panic!("#[qslot] proc macro called outside #[qobject] or #[qobject_impl].")
 }
 
-mod tests {
-    #[test]
-    fn test_qproperty_syntax() {
-
-        assert_eq!(
-            qproperty!("value", Read = get_value, Notify = "valueChanged"),
-            ["value", "", "get_value", "", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Read = get_value, Write = set_value, Notify = "valueChanged"),
-            ["value", "", "get_value", "set_value", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Read = get_value, Write = set_value, Notify = "valueChanged", Constant),
-            ["value", "", "get_value", "set_value", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Member = value_prop, Notify = "valueChanged"),
-            ["value", "value_prop", "", "", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Member = value_prop, Notify = "valueChanged", Constant),
-            ["value", "value_prop", "", "", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Member = value_prop, Read = get_value, Notify = "valueChanged"),
-            ["value", "value_prop", "get_value", "", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Member = value_prop, Write = set_value, Notify = "valueChanged"),
-            ["value", "value_prop", "", "set_value", "valueChanged"]);
-
-        assert_eq!(
-            qproperty!("value", Member = value_prop, Read = get_value, Write = set_value, Notify = "valueChanged"),
-            ["value", "value_prop", "get_value", "set_value", "valueChanged"]);
-
-    }
+#[proc_macro]
+pub fn qproperty(_: TokenStream) -> TokenStream {
+    // This macro does nothing but offer an entry point for Rust doc
+    panic!("qproperty! macro called outside #[qobject] or #[qobject_impl].");
 }
