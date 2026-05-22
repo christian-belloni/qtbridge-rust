cecca66ac44a9dc48794fde324250aaf4830ab92
Juha Vuolle <juha.vuolle@qt.io>
2026-05-22 09:26:19 +0300

    Fix tst_generic_itemmodel autotest on macOS
    macOS Cocoa requires that UI code runs in the main / first
    thread.
    
    Using testlib / cargo test harness violates this because the
    testharness itself is the first thread, and the actual test is
    run in a separate thread. The result is a crash / exception by
    the operating system.
    
    Change the test to a self-contained binary with its own main
    function so that it won't be run in a harness.
    
    The change requires a bit more moving around the code than
    one would perhaps expect; this is because the test contains
    in fact both autotest and manual test -> we need to put the
    shared code into its own file so that both tests can refer
    to it.
    
    Task-number: QTBRIDGES-188
    Change-Id: I1c29513364f467b1d43f962a3b9390b251504503
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>

--- test/quick/tst_generic_itemmodel/src/main.rs
+++ test/quick/tst_generic_itemmodel/src/main.rs
@@ -3,100 +3,10 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use qtbridge::{qobject, QApp};
+use qtbridge::QApp;
 use qtbridge::QObjectHolder;
 
-#[qobject(Base = QAbstractItemModel)]
-mod backend {
-    use qtbridge::qtbridge_type_lib::{QVariant, QModelIndex};
-    use qtbridge::{QAbstractItemModel, QAbstractItemModelBase};
-
-    #[derive(Default)]
-    pub struct Backend<T>
-    where T: 'static + Default,
-        for<'a> qtbridge::qtbridge_type_lib::QVariant: From<&'a T>, // TODO: make it work without fully qualified path for QVariant
-    {
-        data: Vec<T>,
-    }
-    impl<T> Backend<T>
-    where T: 'static + Default,
-        for<'a> QVariant: From<&'a T>,
-    {
-        pub fn new(data: Vec<T>) -> Self {
-            Self {
-                data: data
-            }
-        }
-    }
-
-    impl<T> QAbstractItemModel for Backend<T>
-        where
-        T: 'static + Default,
-        for<'a> qtbridge::qtbridge_type_lib::QVariant: From<&'a T>,
-    {
-
-        fn index(&self, row: i32, column: i32, _parent: &QModelIndex) -> QModelIndex {
-            self.create_index(row, column, 0)
-        }
-
-        fn parent(&self, _child: &QModelIndex) -> QModelIndex {
-            QModelIndex::default()
-        }
-
-        fn row_count(&self, parent: &QModelIndex) -> i32 {
-            if !parent.is_valid() {
-                self.data.len() as i32
-            } else {
-                0
-            }
-        }
-
-        fn column_count(&self, _parent: &QModelIndex) -> i32 {
-            1
-        }
-
-        fn data(&self, index: &QModelIndex, _role: i32) -> QVariant {
-            QVariant::from(&self.data[index.row() as usize])
-        }
-
-        fn set_data(&mut self, _index: &QModelIndex, _value: &QVariant, _role: i32) -> bool {
-            false
-        }
-    }
-
-}
-
-use backend::Backend;
-
-#[test]
-fn test_qabstractitemmodel() {
-    use std::env;
-    use std::path::PathBuf;
-
-    let mut input_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-
-    input_path.push("qml");
-    let input_folder = input_path.to_str().unwrap().to_string();
-
-    println!("Running quick test with qml files in \"{}\"", &input_folder);
-
-    let args: Vec<String> = vec![
-        file!().to_string(),
-        "-input".to_string(),
-        input_folder,
-    ];
-
-    use qtbridge::qtbridge_type_lib::QVariantMap;
-    use quicktest::quick_test_main_with_properties;
-    let data = vec![1, 2, 3, 10, 100];
-    let test_object = Rc::new(RefCell::new(Backend::<i32>::new(data)));
-    Backend::attach_qobject(&test_object);
-
-    let properties = QVariantMap::from(("listmodel", test_object.borrow().as_qvariant()));
-    let result = quick_test_main_with_properties(&args, &"test_qabstractitemmodel".to_string(), &properties);
-
-    assert_eq!(result, 0, "quick_test failed with code {}", result);
-}
+use tst_generic_itemmodel::Backend;
 
 //Manual test
 fn main() {
---
+++ test/quick/tst_generic_itemmodel/tests/qabstractitemmodel.rs
@@ -0,0 +1,5 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
+fn main() {
+    tst_generic_itemmodel::test_qabstractitemmodel();
+}
