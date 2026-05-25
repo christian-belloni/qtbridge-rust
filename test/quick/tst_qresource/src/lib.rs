cc07524698a6879bf8161c0031b886f2177a81c5
Juha Vuolle <juha.vuolle@qt.io>
2026-05-25 15:03:14 +0300

    Fix tst_qresource on macOS
    By removing the testlib / cargo harness use, see previous commits
    for details. In short allow UI to run on first/main thread as
    required by macOS / Cocoa.
    
    Task-number: QTBRIDGES-188
    Change-Id: I0964c79cf787bd205740071333eca5b68557a090
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>

---
+++ test/quick/tst_qresource/src/lib.rs
@@ -0,0 +1,31 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
+
+use qtbridge::include_bytes_qml;
+
+pub fn test_qresource() {
+    use std::env;
+    use std::path::PathBuf;
+
+    include_bytes_qml!("example.txt");
+    include_bytes_qml!("text/example.txt");
+    include_bytes_qml!("example.txt", "samefolder");
+    include_bytes_qml!("text/example.txt", "subfolder");
+    include_bytes_qml!("example.txt", "samefolder/addedfolder");
+    include_bytes_qml!("text/example.txt", "subfolder/addedfolder");
+
+    let mut input_path: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    input_path.push("qml");
+    let input_folder = input_path.to_string_lossy().to_string();
+
+    println!("Running quick test with qml files in \"{}\"", &input_folder);
+
+    let args: Vec<String> = vec![
+        file!().to_string(),
+        "-input".into(),
+        input_folder,
+    ];
+
+    let result = quicktest::quick_test_main(&args, &"test_qresource".into());
+    assert_eq!(result, 0, "quick_test failed with code {}", result);
+}
--- test/quick/tst_qresource/src/main.rs
+++ test/quick/tst_qresource/src/main.rs
@@ -3,34 +3,6 @@
 
 use qtbridge::{include_bytes_qml, QApp};
 
-#[test]
-fn test_qresource() {
-    use std::env;
-    use std::path::PathBuf;
-
-    include_bytes_qml!("example.txt");
-    include_bytes_qml!("text/example.txt");
-    include_bytes_qml!("example.txt", "samefolder");
-    include_bytes_qml!("text/example.txt", "subfolder");
-    include_bytes_qml!("example.txt", "samefolder/addedfolder");
-    include_bytes_qml!("text/example.txt", "subfolder/addedfolder");
-
-    let mut input_path: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    input_path.push("qml");
-    let input_folder = input_path.to_string_lossy().to_string();
-
-    println!("Running quick test with qml files in \"{}\"", &input_folder);
-
-    let args: Vec<String> = vec![
-        file!().to_string(),
-        "-input".into(),
-        input_folder,
-    ];
-
-    let result = quicktest::quick_test_main(&args, &"test_qresource".into());
-    assert_eq!(result, 0, "quick_test failed with code {}", result);
-}
-
 fn main() {
 
     // Images from https://rustacean.net/ by Karen Rustad Tölva.
